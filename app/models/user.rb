d08d0603325c97918a4f97d4b2b1e6fc73abe14e
Gary Hall <guategary@gmail.com>
2017-11-08 14:29:16 -0500

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
 end
